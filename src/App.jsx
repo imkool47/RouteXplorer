8d2992883e056437a50be2f4f39575500c87b83b
Mukul Kumar <mukul.kumar630@gmail.com>
2025-01-31 17:14:56 +0530

    feat: added Admin component and enhance routing; improve styling for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { ProductList } from "./components/ProductList";
 import { ProductDetail } from "./components/ProductDetail";
 import { Contact } from "./components/Contact";
+import { Admin } from "./components/Admin";
 import { Footer } from "./components/Footer";
 
 import "./App.css";
 
 function App() {
+  const user = true;
+
   return (
     <div className="App">
       <Header />
@@ -16,8 +19,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="products" element={<ProductList />}></Route>
-          <Route path="products/1001" element={<ProductDetail />}></Route>
+          <Route path="products/:id" element={<ProductDetail />}></Route>
           <Route path="contact" element={<Contact />}></Route>
+          <Route
+            path="admin"
+            element={user ? <Admin /> : <Navigate to="/" />}
+          ></Route>
         </Routes>
       </main>
       <Footer />
